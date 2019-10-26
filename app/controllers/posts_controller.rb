8b4942253ecafdddbb4d47cf676ece80ebc62bbb
Lillian Luzinsky <eyeris@tpg.com.au>
2019-10-26 15:11:33 +1100

    updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_filter :authorize, only: [:edit, :update]
+    # before_filter :authorize, only: [:edit, :update]
     def index
         
     end
