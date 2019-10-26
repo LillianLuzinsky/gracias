4398819260ae7bceec26123d2d085ef60dea4428
Lillian Luzinsky <eyeris@tpg.com.au>
2019-10-26 16:13:43 +1100

    fixed logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'login', to: 'sessions#destroy', as: 'logout'
-  
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
   resources :sessions
 end
